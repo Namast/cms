163397e291976b0f5424b1de991b7ab075fd92cd
Syrtsov Andrey <namastua@gmail.com>
2019-07-08 19:03:21 +0300

    шаблонный фильтр

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,8 +7,14 @@ register = template.Library()
 
 @register.inclusion_tag('blog/tags/category.html')
 def category_list(publish=True):
+    """Template Tag вывода категорий"""
     category = Category.objects.filter(active=publish)
     return {"category_list": category}
 
 
+
+@register.filter()
+def comment_padding(value):
+    """Filter сдвига комментария"""
+    return int(value / 2) == float(value / 2)
 
